d7655848672519fce3238dba08ed4c1cb09831ee
Akinur Rahman <129282346+akinurrahman@users.noreply.github.com>
2023-07-08 23:42:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+  // section hover
+  let sections = document.querySelectorAll('section');
+  let navLinks = document.querySelectorAll('header nav a');
+
+  window.onscroll = () => {
+    sections.forEach((sec) => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+
+      if (top >= offset && top < offset + height) {
+        navLinks.forEach((links) => {
+          links.classList.remove('active');
+          document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        });
+      }
+    });
+    // sticky navbar
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+  };
+// responsive navbar
+const menu = document.querySelector('.hamburger-container')
+const hamburger = document.querySelector('.fa-solid')
+const nav = document.querySelector('.navbar')
+menu.addEventListener('click',()=>{
+hamburger.classList.toggle('fa-xmark')
+if(hamburger.contains="fa-xmark"){
+  nav.classList.toggle('active')
+}
+})
